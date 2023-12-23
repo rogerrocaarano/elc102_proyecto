cd6749433b4bcc7e1a0ed5e968f19aadf8347110
Róger Roca Arano <rogerroca@reinseg.com>
2023-12-22 23:56:15 -0400

    Documentación.

--- package/Canvas/DrawEvent.py
+++ package/Canvas/DrawEvent.py
@@ -13,10 +13,20 @@ class DrawEvent:
         self.points = []
 
     def draw_line_action_triggered(self):
+        """
+        Triggered when the user clicks on the draw line action
+        :return:
+        """
         self.drawing = "line"
         self.initial_point = None
 
     def draw_line_event(self, canvas, ev):
+        """
+        Triggered when the user clicks on the canvas while drawing a line
+        :param canvas: Canvas object where the line will be drawn
+        :param ev: Event object
+        :return:
+        """
         if self.initial_point is None:
             self.initial_point = ev.pos()
         else:
@@ -25,10 +35,20 @@ class DrawEvent:
             self.drawing = None
 
     def draw_square_action_triggered(self):
+        """
+        Triggered when the user clicks on the draw square action
+        :return:
+        """
         self.drawing = "square"
         self.initial_point = None
 
     def draw_square_event(self, canvas, ev):
+        """
+        Triggered when the user clicks on the canvas while drawing a square
+        :param canvas: Canvas object where the square will be drawn
+        :param ev: Event object
+        :return:
+        """
         if self.initial_point is None:
             self.initial_point = ev.pos()
         else:
@@ -38,10 +58,20 @@ class DrawEvent:
             self.drawing = None
 
     def draw_rectangle_action_triggered(self):
+        """
+        Triggered when the user clicks on the draw rectangle action
+        :return:
+        """
         self.drawing = "rectangle"
         self.initial_point = None
 
     def draw_rectangle_event(self, canvas, ev):
+        """
+        Triggered when the user clicks on the canvas while drawing a rectangle
+        :param canvas: Canvas object where the rectangle will be drawn
+        :param ev: Event object
+        :return:
+        """
         if self.initial_point is None:
             self.initial_point = ev.pos()
         else:
@@ -51,10 +81,20 @@ class DrawEvent:
             self.drawing = None
 
     def draw_triangle_action_triggered(self):
+        """
+        Triggered when the user clicks on the draw triangle action
+        :return:
+        """
         self.drawing = "triangle"
         self.initial_point = None
 
     def draw_triangle_event(self, canvas, ev):
+        """
+        Triggered when the user clicks on the canvas while drawing a triangle
+        :param canvas: Canvas object where the triangle will be drawn
+        :param ev: Event object
+        :return:
+        """
         if not self.points:
             self.points.append(ev.pos())
         elif len(self.points) < 2:
@@ -66,10 +106,21 @@ class DrawEvent:
             self.points = []
 
     def draw_polygon_action_triggered(self):
+        """
+        Triggered when the user clicks on the draw polygon action
+        :return:
+        """
         self.drawing = "polygon"
         self.initial_point = None
 
     def draw_polygon_event(self, canvas, ev, close_signal=False):
+        """
+        Triggered when the user clicks on the canvas while drawing a polygon
+        :param canvas: Canvas object where the polygon will be drawn
+        :param ev: Event object
+        :param close_signal: If True, the polygon will be closed
+        :return:
+        """
         if close_signal and len(self.points) > 2:
             self.points.append(self.points[0])
             polygon = Polygon(self.points)
